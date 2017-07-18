53ec85761f4851798c2e3b231c4912c392203c8d
Aodaruma <skyblue.1007.sakkyoku@gmail.com>
2017-07-18 15:04:25 +0900

    added php to switch different laungages page and deleted index.html

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+// 言語振り分け
+$languages = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+$languages = array_reverse($languages);
+ 
+$result = '';
+ 
+foreach ($languages as $language) {
+    if(preg_match('/^en/i', $language)){
+        $result = 'English';
+        header("Location: /en/");
+    }elseif (preg_match('/^ja/i', $language)){
+        $result = 'Japanese';
+        header("Location: /jp/");
+    }
+}
+if($result == '') {
+    $result = 'Japanese';
+    header("Location: /jp/");
+}
+?>
